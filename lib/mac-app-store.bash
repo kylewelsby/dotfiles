f054c44f2b023ea36d4e3de6011de4ac709c6ae2
Kyle Welsby <kyle@mekyle.com>
2019-01-16 19:58:23 +0000

    App Store check for authentication

--- lib/mac-app-store.bash
+++ lib/mac-app-store.bash
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-echo -e "\033[0;32mInstalling App Store Applications\033[0m"
-for pname in $APP_STORE_PROGRAMS; do
- run "mas lucky ${pname}"
-done
+mas account
+if [ $? -ne 0 ]; then
+  echo "\033[0;31mApp Store not Signed in\033[0m"
+else
+  echo -e "\033[0;32mInstalling App Store Applications\033[0m"
+  for pname in $APP_STORE_PROGRAMS; do
+   run "mas lucky ${pname}"
+  done
+fi
