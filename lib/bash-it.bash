97a645201d71a8163a4a9c95fccf94410576aaf6
Kyle Welsby <kyle@mekyle.com>
2019-01-16 17:46:11 +0000

    Fixes bug with bash-it exiting early
    Found two issues with the master branch of bash-it, so issued bugfixes and using my branch for now.

--- lib/bash-it.bash
+++ lib/bash-it.bash
@@ -1,27 +1,44 @@
 #!/usr/bin/env bash
 
+### To uninstall ###
+#
+# $ ~/.bash_it/uninstall.sh && rm -rf ~/.bash_it
+#
+### end ###
+
 source ./lib/heleprs.bash
 
-bash_it_git="Bash-it/bash-it"
+# bash_it_git="Bash-it/bash-it"
+# bash_it_branch="master"
+bash_it_git="kylewelsby/bash-it"
+bash_it_branch="bugfix/missing-tmp-cache-exit-1"
 bash_it_location="${HOME}/.bash_it"
-bash_it_clone="$git_command clone --depth=1 https://github.com/$bash_it_git.git ${bash_it_location}"
+bash_it_clone="$git_command clone --depth=1 --branch=${bash_it_branch} https://github.com/$bash_it_git.git ${bash_it_location}"
 bash_it_install="${HOME}/.bash_it/install.sh --silent --no-modify-config"
 
 if [ ! -d $bash_it_location ]; then
+  echo ""
   echo -e "\033[0;32mInstalling Bash-it\033[0m"
   run "rm -rf ${bash_it_location}"
   run "$bash_it_clone"
   run "$bash_it_install"
 fi
 
-export BASH_IT=~/.bash_it
+if [ -z "$BASH_IT" ];
+then
+  BASH_IT="$HOME/.bash_it"
+fi
 
+echo ""
 echo -e "\033[0;32mEnabling Bash-it aliases, completions, and plugins\033[0m"
 
-source ${bash_it_location}/lib/composure.bash
-source ${bash_it_location}/lib/utilities.bash
+source ~/.bash_it/lib/composure.bash
+source ~/.bash_it/lib/utilities.bash
 cite _about _param _example _group _author _version
-source ${bash_it_location}/lib/helpers.bash
+source ~/.bash_it/lib/helpers.bash
+
+run "_bash-it-version"
+
 run "bash-it enable alias ${BASH_IT_ALIAS}"
 run "bash-it enable completion ${BASH_IT_COMPLETIONS}"
 run "bash-it enable plugin ${BASH_IT_PLUGINS}"
