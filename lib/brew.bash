7ab70f9c06d30937b117526394a1c6b33a7f3873
Kyle Welsby <kyle@mekyle.com>
2019-01-18 15:05:47 +0000

    Fixes #2 brew exiting script when not installed

--- lib/brew.bash
+++ lib/brew.bash
@@ -2,12 +2,14 @@
 
 source ./lib/heleprs.bash
 
+set +e
 which brew
 if [[ $? != 0 ]]; then
   echo -e "\033[0;32mInstalling Homebrew\033[0m"
   brew_install='/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"'
   run "$brew_install"
 fi
+set -e
 
 brew_command=`which brew`
 
