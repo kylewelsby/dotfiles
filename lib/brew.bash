00a6de1ff02bda6b3be25df64a847e80ef8e005c
Kyle Welsby <kyle@mekyle.com>
2019-10-31 22:43:24 +0000

    Update brew in the routine

--- lib/brew.bash
+++ lib/brew.bash
@@ -12,7 +12,10 @@ fi
 set -e
 
 brew_command=`which brew`
-
+echo -e "\033[0;32mUpdating Homebrew\033[0m"
+run "${brew_command} --version"
+run "${brew_command} update"
+run "${brew_command} --version"
 echo -e "\033[0;32mInstalling Brew Libraries\033[0m"
 run "${brew_command} install ${BREW_PROGRAMS} || true"
 run "${brew_command} cleanup"
