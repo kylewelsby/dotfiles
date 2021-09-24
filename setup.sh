2e07987b948733fb80d6d00698d6e87f35daea15
Kyle Welsby <kyle.welsby@shopify.com>
2021-09-24 23:13:49 +0100

    🔑update git gpg key

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 
+git config --global gpg.program gpg
+git config --global user.email kyle.welsby@shopify.com
+
 gpgconf --launch dirmngr
 gpg --keyserver keys.openpgp.org --recv C35CD0258F1362121AE40479AEE59E1B8CCD1C8D
