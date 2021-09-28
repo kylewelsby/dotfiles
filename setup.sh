0705ae54eea1017b7a8c72bf2eda843096c520cf
Kyle Welsby <kyle@mekyle.com>
2021-09-28 11:26:15 +0100

    update signing key

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@ ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 
 git config --global gpg.program gpg
 git config --global user.email kyle.welsby@shopify.com
+git config --global user.signingkey AEE59E1B8CCD1C8D
 
 gpgconf --launch dirmngr
 gpg --keyserver keys.openpgp.org --recv C35CD0258F1362121AE40479AEE59E1B8CCD1C8D
