b25eddb3bcbb1da54cd45b6db9edc6150b9d346e
Kyle Welsby <kyle@mekyle.com>
2021-10-18 12:31:12 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,4 +5,4 @@ git config --global user.email kyle.welsby@shopify.com
 git config --global user.signingkey AEE59E1B8CCD1C8D
 
 gpgconf --launch dirmngr
-gpg --keyserver keys.openpgp.org --recv C35CD0258F1362121AE40479AEE59E1B8CCD1C8D
+gpg --keyserver keyserver.ubuntu.com --recv C35CD0258F1362121AE40479AEE59E1B8CCD1C8D
