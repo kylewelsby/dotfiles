e7a39ec6fb694b6591374d17d0e517ea6e6d5448
Kyle Welsby <kyle.welsby@shopify.com>
2022-03-09 17:17:20 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,7 @@
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 
-gpgconf --launch dirmngr
-gpg --keyserver keyserver.ubuntu.com --recv C35CD0258F1362121AE40479AEE59E1B8CCD1C8D
-
 git config --global gpg.program gpg
 git config --global user.email kyle.welsby@shopify.com
-git config --global user.signingkey 8CCD1C8D
-
 
 gpgconf --launch dirmngr
 
